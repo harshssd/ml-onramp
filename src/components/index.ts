39901f6567d6eb5450d9c34129171085f01e5b8a
hyperfocused <hyperfocusedengineer@gmail.com>
2025-09-21 03:18:42 -0700

    Fix component exports in index file

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export { AuthForm } from './AuthForm';
-export { LessonCard, ProgressRing } from './LessonCard';
+export { LessonCard } from './LessonCard';
+export { ProgressRing } from './ProgressRing';
 export { RegressionPlayground } from './RegressionPlayground';
 export type { Lesson } from './LessonCard';
